566ef35f90076242360c9361fe6a5f03276c8d43
Lorenzo-Seghesio <lorenzo.seghesio@kuleuven.be>
2025-07-24 15:20:48 +0200

    Organization of the code, putting old code in a separated folder.

--- Data_Converter.py
+++ Data_Converter.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_parquet('data/dataset.parquet')
 print(df.head(10))
-# df.to_csv('data/data_fraunhofer.csv', index=False)
+df.to_csv('data/data_fraunhofer.csv', index=False)
